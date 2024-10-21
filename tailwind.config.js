5b53265873945aa07497a2e306f1aef64050ecf9
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-10-21 14:52:04 +0700

    updated framework react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
-module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      padding: '16px',
+// import daisyui from 'daisyui'
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#810000',
+                secondary: '#EEEBDD',
+                dark: '#1B1717'
+            },
+            gridTemplateColumns: {
+                'auto-1fr': 'auto 1fr',
+              },
+            screens: {
+                '2xl': '1320px'
+            }
+        },
     },
-    extend: {
-      colors: {
-        primary: '#14b8a6',
-        secondary: '#64748b',
-        dark: '#0f172a',
-      },
-      screens: {
-        '2xl': '1320px',
-      },
-    },
-  },
-  plugins: [],
-};
+    plugins: [
+        'daisyui',
+      ],
+}
+
+
