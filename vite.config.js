135718add1d6a4785ba92184473b3dd77c3161cc
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-10-23 14:31:06 +0700

    Edited

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Portofolio/",
   plugins: [react()],
-  plugins: [visualizer()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // mengelompokkan semua dependency node_modules ke dalam chunk 'vendor'
-          }
-        }
-      }
-    }
-  },
 })
-
